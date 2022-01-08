da415c70437fc7bce082f7a7539539ff627309c8
Michael Babin <mic.babin@outlook.com>
2022-01-08 17:22:15 -0500

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FeatureModule } from './feature/feature.module';
 
 @NgModule({
-  declarations: [
-    // Components, pipes and custom directives
-    AppComponent,
-  ],
-  imports: [
-    // Other modules
-    BrowserModule,
-    AppRoutingModule,
-  ],
-  providers: [
-    // Services and guards
-  ],
-  bootstrap: [
-    // Component loaded at start of the application
-    AppComponent,
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, FeatureModule],
+  providers: [],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
